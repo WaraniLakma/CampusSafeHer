9485911631b8b04456c7a2f849fdd3b39eebfc0a
Warani Lakma <lakmasiri7@gmail.com>
2026-06-15 00:42:56 +0530

    Connect trusted contacts page to backend

--- frontend/src/pages/Contacts.jsx
+++ frontend/src/pages/Contacts.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+import API from "../services/api";
+
 function Contacts() {
-  return <h1>Trusted Contacts</h1>;
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    fetchContacts();
+  }, []);
+
+  const fetchContacts = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const res = await API.get("/contacts", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setContacts(res.data.contacts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Trusted Contacts</h1>
+
+      {contacts.map((contact) => (
+        <div key={contact._id}>
+          <h3>{contact.name}</h3>
+          <p>{contact.phone}</p>
+          <p>{contact.relationship}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Contacts;
