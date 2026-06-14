052ada4683e413b515438193c916a8886c11dc8c
Warani Lakma <lakmasiri7@gmail.com>
2026-06-14 22:48:48 +0530

    Connect frontend login to backend API

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,61 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import API from "../services/api";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await API.post("/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login Successful!");
+
+      navigate("/dashboard");
+    } catch (error) {
+      alert(error.response?.data?.message || "Login Failed");
+    }
+  };
+
   return (
     <div>
       <h1>CampusSafeHer Login</h1>
+
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">
+          Login
+        </button>
+      </form>
     </div>
   );
 }
